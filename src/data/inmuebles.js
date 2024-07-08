264dadaad5950b5ad178f26e2100bd6586caa583
Jose-Mario-Vargas <josevaragsg75@gmail.com>
2024-07-08 17:07:14 -0600

    lista inmuebles

---
+++ src/data/inmuebles.js
@@ -0,0 +1,49 @@
+const inmuebles = [
+    {
+        codigo:"f001",
+        url: 'Apartamento-amueblado1',
+        nombre: 'Apartamento amueblado',
+        medidas: '150 M2 - 214.7 V2',
+        habitaciones: 3,
+        estacionamiento: 1,
+        precio: "70.000",
+        disponible: true,
+        imagen: 'https://images.pexels.com/photos/2030037/pexels-photo-2030037.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+        codigo:"f002",
+        url: 'Apartamento-amueblado2',
+        nombre: 'Apartamento amueblado',
+        medidas: '150 M2 - 214.7 V2',
+        habitaciones: 3,
+        estacionamiento: 1,
+        precio: "70.000",
+        disponible: true,
+        imagen: 'https://images.pexels.com/photos/271795/pexels-photo-271795.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+        codigo:"f003",
+        url: 'Apartamento-amueblado3',
+        nombre: 'Apartamento amueblado',
+        medidas: '150 M2 - 214.7 V2',
+        habitaciones: 3,
+        estacionamiento: 1,
+        precio: "70.000",
+        disponible: false,
+        imagen: 'https://images.pexels.com/photos/323775/pexels-photo-323775.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+    {
+        codigo:"f004",
+        url: 'Apartamento-amueblado4',
+        nombre: 'Apartamento amueblado',
+        medidas: '150 M2 - 214.7 V2',
+        habitaciones: 3,
+        estacionamiento: 1,
+        precio: "70.000",
+        disponible: false,
+        imagen: 'https://images.pexels.com/photos/2251247/pexels-photo-2251247.jpeg?auto=compress&cs=tinysrgb&w=600',
+    },
+]
+
+
+export default inmuebles
